9270b61595b26d8188f07a9afd49a5334b8398fa
Blacketermatthew <53919279+Blacketermatthew@users.noreply.github.com>
2021-04-08 21:58:31 -0500

    App is effectively done.  Finished section 5 in lab.

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or \
+    b'\x18[\xa9:l\x83\xedB\x1aQD!l\x82-\xdb'
 
     MONGODB_SETTINGS = { 'db': 'UTA_Enrollment' }
